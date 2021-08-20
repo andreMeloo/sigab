2ccf2134106b8266c793f9258c6e15de42e65c30
Davi Guimarães <ddaavvii2001@gmail.com>
2021-08-19 22:18:35 -0300

    refact. validações em UsuarioVO

---
+++ src/enums/NivelDeUsuario.java
@@ -0,0 +1,5 @@
+package enums;
+
+public enum NivelDeUsuario {
+    ALUNO, PROFESSOR, ADMIN
+}
--- src/model/vo/UsuarioVO.java
+++ src/model/vo/UsuarioVO.java
@@ -1,9 +1,12 @@
 package model.vo;
 
+import enums.NivelDeUsuario;
+
 class UsuarioVO {
+
 	private long id;
 	private String nome;
-	private int nivel;
+	private NivelDeUsuario nivel;
 	private String username;
 	private String senha;
 
@@ -11,44 +14,44 @@ class UsuarioVO {
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		if(id > 0)
-		this.id = id;
+			this.id = id;
 	}
-
 
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		if(nome != null)
-		this.nome = nome;
+			this.nome = nome;
 	}
 
-
-	public int getNivel() {
+	public NivelDeUsuario getNivel() {
 		return nivel;
 	}
-	public void setNivel(int nivel) {
-		if(nivel >= 0 && nivel <= 2)
+
+	public void setNivel(NivelDeUsuario nivel) {
 		this.nivel = nivel;
 	}
-
 
 	public String getUsername() {
 		return username;
 	}
-	public void setUsername(String username) {
-		if(username != null && username != (""))
-		this.username = username;
-	}
 
+	public void setUsername(String username) {
+		if (username != null && username.equals(""))
+			this.username = username;
+	}
 
 	public String getSenha() {
 		return senha;
 	}
+
 	public void setSenha(String senha) {
-		if(senha != null && senha != (""))
-		this.senha = senha;
+		if(senha != null && senha.length() >= 6)
+			this.senha = senha;
 	}
 }
