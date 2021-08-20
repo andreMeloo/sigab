63d9cad671466c14d08e03fd0b5353d55281a714
Guilherme <guilhermesaldanha08@gmail.com>
2021-08-19 21:58:21 -0300

    fix: indentation

--- src/model/vo/DisciplinaVO.java
+++ src/model/vo/DisciplinaVO.java
@@ -9,14 +9,14 @@ public class DisciplinaVO {
 	}
 	public void setCodigo(long codigo) {
 		if(codigo > 0)
-		this.codigo = codigo;
+			this.codigo = codigo;
 	}
 	public String getNome() {
 		return nome;
 	}
 	public void setNome(String nome) {
 		if(nome != null && !nome.equals(""))
-		this.nome = nome;
+			this.nome = nome;
 	}
 	
 }
