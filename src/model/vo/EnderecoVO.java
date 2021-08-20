91e884aafed3cc511c174ebb68945f92a66fe135
Davi Guimarães <ddaavvii2001@gmail.com>
2021-08-19 22:30:55 -0300

    refact. herança EnderecoVO

--- src/model/vo/EnderecoVO.java
+++ src/model/vo/EnderecoVO.java
@@ -1,6 +1,7 @@
 package model.vo;
 
 public class EnderecoVO {
+	
 	private long id;
 	private String rua;
 	private String cidade;
@@ -9,29 +10,36 @@ public class EnderecoVO {
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
-		if(id>0)
+		if(id > 0)
 			this.id = id;
 	}
+
 	public String getRua() {
 		return rua;
 	}
+
 	public void setRua(String rua) {
-		if(rua!=null && !rua.equals(""))
+		if(rua != null && !rua.equals(""))
 			this.rua = rua;
 	}
+
 	public String getCidade() {
 		return cidade;
 	}
+
 	public void setCidade(String cidade) {
-		if(cidade!=null && !cidade.equals(""))
+		if(cidade != null && !cidade.equals(""))
 			this.cidade = cidade;
 	}
+
 	public String getUf() {
 		return uf;
 	}
+
 	public void setUf(String uf) {
-		if(uf!=null && !uf.equals(""))
+		if(uf != null && uf.length() > 0 && uf.length() <= 2)
 			this.uf = uf;
 	}
 }
