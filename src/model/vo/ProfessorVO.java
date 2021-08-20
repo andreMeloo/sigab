20569204e660d537d1125eb0dc9347121609d7f3
Davi Guimarães <ddaavvii2001@gmail.com>
2021-08-19 21:37:32 -0300

    feat. implementação de ProfessorVO e TurmaVO

--- src/model/vo/ProfessorVO.java
+++ src/model/vo/ProfessorVO.java
@@ -2,4 +2,23 @@ package model.vo;
 
 public class ProfessorVO {
     
+    private String cpf;
+    private Endereco endereco;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
 }
--- src/model/vo/TurmaVO.java
+++ src/model/vo/TurmaVO.java
@@ -1,5 +1,63 @@
 package model.vo;
 
+import java.util.List;
+
 public class TurmaVO {
+    
+    private long id;
+    private String horario;
+    private String local;
+
+    private DisciplinaVO disciplina;
+    private List<AlunoVO> alunos;
+    private ProfessorVO professor;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    public DisciplinaVO getDisciplina() {
+        return disciplina;
+    }
+
+    public void setDisciplina(DisciplinaVO disciplina) {
+        this.disciplina = disciplina;
+    }
+
+    public List<AlunoVO> getAlunos() {
+        return alunos;
+    }
+    
+    public void setAlunos(List<AlunoVO> alunos) {
+        this.alunos = alunos;
+    }
+
+    public ProfessorVO getProfessor() {
+        return professor;
+    }
+    
+    public void setProfessor(ProfessorVO professor) {
+        this.professor = professor;
+    }
     
 }
