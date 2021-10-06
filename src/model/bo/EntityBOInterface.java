019b0ac989a9e01b6417c1b79475766eea5ca968
GuilhermeA08 <guilhermesaldanha08@gmail.com>
2021-10-05 22:38:33 -0300

    feat: create EntityBOInterface

---
+++ src/model/bo/EntityBOInterface.java
@@ -0,0 +1,10 @@
+package model.bo;
+
+import java.util.List;
+
+public interface EntityBOInterface<ClassVO> {
+    public void salvar(ClassVO classVO);
+    public List<ClassVO> listar();
+    public void remover(ClassVO classVO);
+    public void editar(ClassVO classVO);
+}
