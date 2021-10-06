3c5446c92c806b5a2c119b9a55a668dfa88dae9d
GuilhermeA08 <guilhermesaldanha08@gmail.com>
2021-10-06 15:07:32 -0300

    feat: Create AlunoBO

---
+++ src/model/bo/AlunoBO.java
@@ -0,0 +1,46 @@
+package model.bo;
+
+import java.util.List;
+
+import model.dao.AlunoDAO;
+import model.vo.AlunoVO;
+
+public class AlunoBO implements EntityBOInterface<AlunoVO>{
+
+    @Override
+    public void salvar(AlunoVO alunoVO) {
+        AlunoDAO alunoDAO = new AlunoDAO();
+        alunoDAO.inserir(alunoVO);
+    }
+
+    @Override
+    public List<AlunoVO> listar() {
+        AlunoDAO alunoDAO = new AlunoDAO();
+        alunoDAO.listar();
+        return null;
+    }
+
+    @Override
+    public void remover(AlunoVO alunoVO) {
+        AlunoDAO alunoDAO = new AlunoDAO();
+        try {
+            alunoDAO.getById(alunoVO.getId());
+            alunoDAO.remover(alunoVO);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+    }
+
+    @Override
+    public void editar(AlunoVO alunoVO) {
+        AlunoDAO alunoDAO = new AlunoDAO();
+        try {
+            alunoDAO.getById(alunoVO.getId());
+            alunoDAO.editar(alunoVO);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+}
