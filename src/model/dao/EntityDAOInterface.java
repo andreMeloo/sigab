e4fa0a3905fef24882c45a817d625f678117a15c
Gabriel Gadelha <41694144+GabrielGadelha@users.noreply.github.com>
2021-10-11 10:46:38 -0300

    Update EntityDAOInterface.java

--- src/model/dao/EntityDAOInterface.java
+++ src/model/dao/EntityDAOInterface.java
@@ -2,7 +2,8 @@ package model.dao;
 
 import java.util.List;
 
-public interface EntityDAOInterface <ClassVO> {
+public interface EntityDAOInterface <ClassVO> { // Provavavelmente vocês viram o padrão entity em algum lugar e acham que tudo que se refere a bd dentro do código java é uma entity.
+    // essa classe é qualquer coisa, menos uma Entity. Leiam o padrão. Caso ainda fiquem com dúvidas, me chamem no discord que explico a vocês.
     public void inserir(ClassVO objectVO);
     public void remover(ClassVO objectVO);
     public List<ClassVO> listar();
