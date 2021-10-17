b9d583ee25e4c626132fba612b10b7f16b44fc32
DaVinci16180 <ddaavvii2001@gmail.com>
2021-10-16 22:59:00 -0300

    feat. classes das telas de admin

---
+++ src/view/Admin/AdminTurmas.java
@@ -0,0 +1,23 @@
+package view.Admin;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class AdminTurmas extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("./turmas.fxml"));
+        Scene cena = new Scene(root);
+
+        primaryStage.setTitle("Turmas");
+        primaryStage.setScene(cena);
+        primaryStage.show();
+    }
+}
