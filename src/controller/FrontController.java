7ab9e8d12a0358771ec2ce795147b1a3c6555c8d
th35ly <programador.sly@gmail.com>
2021-10-16 21:29:24 -0300

    create. package controller e class FrontController

--- src/controller/FrontController.java
+++ src/controller/FrontController.java
@@ -1,9 +1,49 @@
 package controller;
 
+import enums.NivelDeUsuario;
+import exception.AuthenticationException;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import model.bo.UsuarioBO;
+import model.vo.UsuarioVO;
 
 public class FrontController {
+
+    @FXML private TextField login;
+    @FXML private TextField senha;
+    @FXML private Label erroAut;
+
+    UsuarioBO usuBO = new UsuarioBO();
+
     public void autenticar(ActionEvent event) {
+        UsuarioVO vo = new UsuarioVO();
+        vo.setUsername(login.getText());
+        vo.setSenha(senha.getText());
+
         
+        try 
+        {
+            UsuarioVO autenticado = usuBO.autenticar(vo);
+            switch (autenticado.getNivel()) {
+                case ALUNO:
+                    // chama tela aluno
+                    break;
+                case PROFESSOR:
+                    // chama tela professor
+                    break;
+                case ADMIN:
+                    // chama tela do admin
+                    break;
+                default:
+                    break;
+            }
+
+        } 
+        catch (AuthenticationException e) {
+            erroAut.setText("Usuario ou Senha invalidos!");
+            erroAut.setVisible(true);
+        }
     }
 }
