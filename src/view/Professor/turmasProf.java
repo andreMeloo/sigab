5c00a05d17c23066615703f01648724e1f27ed66
th35ly <programador.sly@gmail.com>
2021-10-20 21:43:45 -0300

    create. package view.Professor com suas telas e classes java diario, perfilProfessor, turmasProf

---
+++ src/view/Professor/turmasProf.java
@@ -0,0 +1,23 @@
+package view.Professor;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class turmasProf extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("./turmasProf.fxml"));
+        Scene cena = new Scene(root);
+
+        primaryStage.setTitle("Perfil");
+        primaryStage.setScene(cena);
+        primaryStage.show();
+    }
+}
